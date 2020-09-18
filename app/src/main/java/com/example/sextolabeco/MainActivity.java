7ac8b74b14161338e98592fc70a77058b092945c
NatalyAM11 <60659258+NatalyAM11@users.noreply.github.com>
2020-09-18 15:58:02 -0500

    Pasoo

--- app/src/main/java/com/example/sextolabeco/MainActivity.java
+++ app/src/main/java/com/example/sextolabeco/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.sextolabeco;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+    private Button bIngresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        username= findViewById(R.id.username);
+        password=findViewById(R.id.password);
+        bIngresar=findViewById(R.id.bIngresar);
+
     }
 }
